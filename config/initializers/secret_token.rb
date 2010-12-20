1a3995a9b16a4b0cfe0c1267b0afa8c9c20a1226
Bramanga <bramanga@gmail.com>
2010-12-19 22:16:06 -0500

    Created initial application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peoplebook::Application.config.secret_token = 'c8982d6a9c8aa6d3cba585a31ff6f61463fb3ab634be350acd79358a78ed7df783dd702690878190285682295f84345739d8f2ffbbd8c93e61a9bc8acbae69be'
