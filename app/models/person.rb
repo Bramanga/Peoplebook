89eb2c244156a41d14faf0f3fa1666d42833da4e
Bramanga <bramanga@gmail.com>
2010-12-29 22:26:42 -0500

    Added the has_attached_file method to the person model.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,6 @@
 class Person < ActiveRecord::Base
+
+	# Adds photo upload option to the Person object
+	has_attached_file :photo
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101220040930) do
+ActiveRecord::Schema.define(:version => 20101230032009) do
 
   create_table "people", :force => true do |t|
     t.string   "f_name"
@@ -22,6 +22,10 @@ ActiveRecord::Schema.define(:version => 20101220040930) do
     t.string   "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
 end
